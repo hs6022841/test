e4adc0a9357ef0aac464471b6eb39f67e55f9f61
jhe <jhe@pulsar.ent.riavera.com>
2019-12-11 14:38:48 -0500

    login done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Faker\Factory as Faker;
 
 class LoginController extends Controller
 {
@@ -35,5 +41,64 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Overriding AuthenticatesUsers->username
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'username';
+    }
+
+    /**
+     * Overriding AuthenticatesUsers->validateLogin
+     *
+     * @param Request $request
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+        ]);
+    }
+
+    /**
+     * Overriding AuthenticatesUsers->attemptLogin
+     *
+     * @param Request $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        // if user does not exists
+        if(!$this->guard()->attempt(
+            $this->credentials($request), $request->filled('remember')
+        )) {
+            Log::info("User " . $request->{$this->username()}. " does not exist, creating new user");
+
+            event(new Registered($user = $this->create($request->all())));
+
+            $this->guard()->login($user);
+        }
+        return true;
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'username' => $data['username'],
+            'name' => Faker::create()->name,
+            // dummy data
+            'password' => Hash::make(''),
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
